a20be3b2c0f4c003086345c2d005dce812f38ab9
AcNas <mikhail@acnas.net>
2025-01-21 23:25:26 +0300

    init

--- src/sgp4_propagator.cpp
+++ src/sgp4_propagator.cpp
@@ -6,79 +6,68 @@ SGP4Propagator::SGP4Propagator(const TLEParser::TLEData& tle) {
 }
 
 void SGP4Propagator::initializeParameters(const TLEParser::TLEData& tle) {
-    // Сохраняем основные элементы
     elements_.epoch = tle.epoch;
+
+    // Преобразуем основные элементы
     elements_.i = tle.inclination * DE2RA;
     elements_.Omega = tle.right_ascension * DE2RA;
     elements_.e = tle.eccentricity;
     elements_.omega = tle.argument_perigee * DE2RA;
     elements_.M = tle.mean_anomaly * DE2RA;
-    elements_.n0 = tle.mean_motion * 2.0 * M_PI / MINUTES_PER_DAY;
+    elements_.n = tle.mean_motion * 2.0 * M_PI / MINUTES_PER_DAY;
     elements_.bstar = tle.bstar;
 
-    // Вычисляем производные параметры
-    elements_.cosio = cos(elements_.i);
-    elements_.sinio = sin(elements_.i);
-
     // Вычисляем большую полуось
-    const double theta2 = elements_.cosio * elements_.cosio;
-    const double x3thm1 = 3.0 * theta2 - 1.0;
-    const double beta0 = sqrt(1.0 - elements_.e * elements_.e);
-
-    double a1 = pow(XKE / elements_.n0, 2.0/3.0);
-    double delta1 = 1.5 * CK2 * x3thm1 / (a1 * a1 * beta0 * beta0 * beta0);
-
-    elements_.a = a1 * (1.0 - delta1/3.0 - delta1 * delta1 - 134.0/81.0 * delta1 * delta1 * delta1);
-    elements_.n = elements_.n0;
+    const double mu = 398600.4418;  // Гравитационный параметр Земли (км³/с²)
+    elements_.a = pow(mu / (elements_.n * elements_.n), 1.0/3.0);
 }
 
-double SGP4Propagator::solveKeplerEquation(double M, double e, int maxIter) const {
-    // Начальное приближение
-    double E = M;
+double SGP4Propagator::solveKepler(double meanAnomaly, double eccentricity) const {
+    double E = meanAnomaly;
+    double delta;
 
-    // Метод Ньютона с ограничением итераций
-    for(int i = 0; i < maxIter; i++) {
-        double delta = (E - e * sin(E) - M) / (1.0 - e * cos(E));
-        E -= delta;
+    // Метод Ньютона с фиксированным числом итераций
+    for(int i = 0; i < 10; i++) {
+        delta = E - eccentricity * sin(E) - meanAnomaly;
+        E = E - delta / (1.0 - eccentricity * cos(E));
 
-        if(fabs(delta) < 1e-12) break;
+        if(fabs(delta) < 1e-12) {
+            break;
+        }
     }
 
     return E;
 }
 
-QVector3D SGP4Propagator::calculatePosition(double xnode, double xinc,
-                                            double xll, double e, double omega,
-                                            double xn) const {
+QVector3D SGP4Propagator::calculatePositionAndVelocity(double tsince) const {
+    // Обновляем среднюю аномалию с учетом времени
+    double M = elements_.M + elements_.n * tsince;
+
     // Решаем уравнение Кеплера
-    double xl = xll + omega;
-    double E = solveKeplerEquation(xl - xnode, e);
+    double E = solveKepler(M, elements_.e);
 
     // Вычисляем истинную аномалию
-    double cosE = cos(E);
     double sinE = sin(E);
-    double beta = sqrt(1.0 - e * e);
-    double f = atan2(beta * sinE, cosE - e);
+    double cosE = cos(E);
+    double nu = atan2(sqrt(1.0 - elements_.e * elements_.e) * sinE,
+                      cosE - elements_.e);
 
-    // Вычисляем расстояние
-    double r = elements_.a * (1.0 - e * cosE);
+    // Вычисляем расстояние до спутника
+    double r = elements_.a * (1.0 - elements_.e * cosE);
 
     // Вычисляем координаты в орбитальной плоскости
-    double cos_f = cos(f);
-    double sin_f = sin(f);
-    double u = omega + f;
-    double cos_u = cos(u);
-    double sin_u = sin(u);
+    double xw = r * (cos(nu + elements_.omega));
+    double yw = r * (sin(nu + elements_.omega));
 
-    // Преобразование в ECI
-    double cos_node = cos(xnode);
-    double sin_node = sin(xnode);
-    double cos_i = cos(xinc);
-    double sin_i = sin(xinc);
+    // Преобразуем в ECI
+    double cosO = cos(elements_.Omega);
+    double sinO = sin(elements_.Omega);
+    double cosI = cos(elements_.i);
+    double sinI = sin(elements_.i);
 
-    double x = r * (cos_node * cos_u - sin_node * sin_u * cos_i);
-    double y = r * (sin_node * cos_u + cos_node * sin_u * cos_i);
-    double z = r * sin_u * sin_i;
+    double x = (xw * cosO - yw * cosI * sinO) * XKMPER;
+    double y = (xw * sinO + yw * cosI * cosO) * XKMPER;
+    double z = (yw * sinI) * XKMPER;
 
     return QVector3D(x, y, z);
 }
@@ -90,32 +79,14 @@ SGP4Propagator::OrbitalState SGP4Propagator::calculateState(const QDateTime& tim
     // Время с эпохи в минутах
     double tsince = elements_.epoch.msecsTo(time) / (1000.0 * 60.0);
 
-    // Вековые возмущения
-    double xnodot = -6.0 * CK2 * elements_.n * elements_.cosio /
-                    (elements_.a * elements_.a * pow(1.0 - elements_.e * elements_.e, 3.5));
+    // Вычисляем позицию в ECI
+    QVector3D pos_vel = calculatePositionAndVelocity(tsince);
+    state.position = pos_vel;
 
-    double omegadot = -3.0 * CK2 * elements_.n * (1.0 - 5.0 * elements_.cosio * elements_.cosio) /
-                      (2.0 * elements_.a * elements_.a * pow(1.0 - elements_.e * elements_.e, 2));
-
-    // Обновляем элементы
-    double xnode = elements_.Omega + xnodot * tsince;
-    double omega = elements_.omega + omegadot * tsince;
-    double xn = elements_.n;
-    double xl = elements_.M + xn * tsince;
-
-    // Вычисляем позицию
-    state.position = calculatePosition(xnode, elements_.i, xl, elements_.e, omega, xn);
-
-    // Вычисляем скорость через конечные разности
-    const double dt = 1.0; // 1 секунда
-    double tsince2 = tsince + dt/60.0;
-
-    double xnode2 = elements_.Omega + xnodot * tsince2;
-    double omega2 = elements_.omega + omegadot * tsince2;
-    double xl2 = elements_.M + xn * tsince2;
-
-    QVector3D pos2 = calculatePosition(xnode2, elements_.i, xl2, elements_.e, omega2, xn);
-    state.velocity = (pos2 - state.position) / dt;
+    // Вычисляем скорость численным методом
+    double dt = 1.0; // 1 секунда
+    QVector3D pos2 = calculatePositionAndVelocity(tsince + dt/60.0);
+    state.velocity = (pos2 - pos_vel) / dt;
 
     return state;
 }
--- src/sgp4_propagator.h
+++ src/sgp4_propagator.h
@@ -23,32 +23,29 @@ private:
     static constexpr double XKE = 7.43669161e-2;
     static constexpr double CK2 = 5.413080e-4;
     static constexpr double CK4 = 0.62098875e-6;
-    static constexpr double XKMPER = 6378.137;
+    static constexpr double XKMPER = 6378.137;          // Радиус Земли (км)
     static constexpr double AE = 1.0;
-    static constexpr double DE2RA = M_PI / 180.0;
+    static constexpr double DE2RA = M_PI / 180.0;       // Градусы в радианы
     static constexpr double MINUTES_PER_DAY = 1440.0;
     static constexpr double OMEGA_E = 7.29211514670698e-5;
+    static constexpr double XMNPDA = 1440.0;            // Минут в день
+    static constexpr double MFACTOR = 7.292115E-5;      // Угловая скорость Земли
 
     struct Elements {
-        double i;          // Наклонение (рад)
-        double Omega;      // Долгота восходящего узла (рад)
+        double a;          // Большая полуось (км)
         double e;          // Эксцентриситет
+        double i;          // Наклонение (рад)
         double omega;      // Аргумент перигея (рад)
+        double Omega;      // Долгота восходящего узла (рад)
         double M;          // Средняя аномалия (рад)
         double n;          // Среднее движение (рад/мин)
         double bstar;      // Баллистический коэффициент
-        QDateTime epoch;   // Эпоха элементов
-
-        // Производные параметры
-        double a;          // Большая полуось (км)
-        double n0;         // Начальное среднее движение
-        double cosio;      // cos(i)
-        double sinio;      // sin(i)
+        QDateTime epoch;   // Эпоха
     };
 
     void initializeParameters(const TLEParser::TLEData& tle);
-    double solveKeplerEquation(double M, double e, int maxIter = 10) const;
-    QVector3D calculatePosition(double xnode, double xinc, double xll, double e, double omega, double xn) const;
+    QVector3D calculatePositionAndVelocity(double tsince) const;
+    double solveKepler(double meanAnomaly, double eccentricity) const;
 
     Elements elements_;
 };

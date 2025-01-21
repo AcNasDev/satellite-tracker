1c64fbf2030fa339aee3c0e78f17ef74fdf87ef1
AcNas <mikhail@acnas.net>
2025-01-21 22:48:01 +0300

    init

--- src/sgp4_propagator.cpp
+++ src/sgp4_propagator.cpp
@@ -18,26 +18,29 @@ void SGP4Propagator::initializeParameters(const TLEParser::TLEData& tle) {
     // Инициализация глубоких элементов
     deep_.cosio = cos(elements_.inclination);
     deep_.sinio = sin(elements_.inclination);
-    const double theta2 = deep_.cosio * deep_.cosio;
-    const double x3thm1 = 3.0 * theta2 - 1.0;
+    deep_.theta2 = deep_.cosio * deep_.cosio;
+    deep_.x3thm1 = 3.0 * deep_.theta2 - 1.0;
     const double eosq = elements_.eccentricity * elements_.eccentricity;
     const double betao2 = 1.0 - eosq;
     const double betao = sqrt(betao2);
 
-    // Коррекция для SGP4
-    const double a1 = pow(XKE / elements_.mean_motion, 2.0/3.0);
-    const double del1 = 1.5 * CK2 * x3thm1 / (a1 * a1 * betao * betao2);
+    // Инициализация SGP4
+    const double a1 = pow(XKE / elements_.mean_motion, TOTHRD);
+    const double del1 = 1.5 * CK2 * deep_.x3thm1 / (a1 * a1 * betao * betao2);
     const double ao = a1 * (1.0 - del1 * (0.5 + del1 * (1.0 + 134.0/81.0 * del1)));
-    const double delo = 1.5 * CK2 * x3thm1 / (ao * ao * betao * betao2);
+    const double delo = 1.5 * CK2 * deep_.x3thm1 / (ao * ao * betao * betao2);
 
     deep_.aodp = ao;
     deep_.xnodp = elements_.mean_motion / (1.0 + delo);
 
-    // Вековые эффекты
-    const double xhdot1 = -1.5 * CK2 * deep_.xnodp * deep_.sinio * deep_.cosio / (betao * betao2);
-    deep_.omgdot = -0.5 * xhdot1 * (5.0 * theta2 - 1.0);
-    deep_.xnodot = xhdot1 * (1.0 - 2.0 * deep_.cosio);
-    deep_.xmdot = deep_.xnodp + 0.5 * xhdot1 * elements_.eccentricity * deep_.sinio;
+    // Расчет секулярных эффектов
+    deep_.omgdot_factor = -1.5 * CK2 * deep_.xnodp * deep_.sinio * deep_.cosio / betao2;
+    deep_.xnodot_factor = deep_.omgdot_factor * deep_.cosio;
+    deep_.xmdot_factor = deep_.xnodp + 0.5 * deep_.omgdot_factor * elements_.eccentricity * deep_.sinio;
+
+    // Инициализация коэффициентов для долгопериодических возмущений
+    deep_.xlcof = 0.125 * CK2 * deep_.sinio * (3.0 + 5.0 * deep_.cosio) / (1.0 + deep_.cosio);
+    deep_.aycof = 0.25 * CK2 * deep_.sinio;
 }
 
 SGP4Propagator::OrbitalState SGP4Propagator::calculateState(const QDateTime& time) const {
@@ -48,63 +51,58 @@ SGP4Propagator::OrbitalState SGP4Propagator::calculateState(const QDateTime& tim
     const double tsince = elements_.epoch.secsTo(time) / 60.0;
 
     // Обновление средних элементов
-    const double xmdf = elements_.mean_anomaly + deep_.xmdot * tsince;
-    const double omgadf = elements_.arg_perigee + deep_.omgdot * tsince;
-    const double xnode = elements_.right_ascension + deep_.xnodot * tsince;
+    const double xmp = elements_.mean_anomaly + deep_.xmdot_factor * tsince;
+    const double omgadf = elements_.arg_perigee + deep_.omgdot_factor * tsince;
+    const double xnode = elements_.right_ascension + deep_.xnodot_factor * tsince;
 
     // Решение уравнения Кеплера
-    double E = xmdf;
+    double E = xmp;
+    double delE;
     for(int i = 0; i < 10; i++) {
-        const double delE = (E - elements_.eccentricity * sin(E) - xmdf) /
-                            (1.0 - elements_.eccentricity * cos(E));
+        delE = (E - elements_.eccentricity * sin(E) - xmp) /
+               (1.0 - elements_.eccentricity * cos(E));
         E -= delE;
         if(fabs(delE) < 1e-12) break;
     }
 
-    // Позиция в орбитальной плоскости
+    // Вычисление позиции в орбитальной плоскости
     const double sinE = sin(E);
     const double cosE = cos(E);
 
+    const double sinv = (sqrt(1.0 - elements_.eccentricity * elements_.eccentricity) * sinE) /
+                        (1.0 - elements_.eccentricity * cosE);
+    const double cosv = (cosE - elements_.eccentricity) / (1.0 - elements_.eccentricity * cosE);
+
     const double r = deep_.aodp * (1.0 - elements_.eccentricity * cosE);
-    const double rdot = deep_.aodp * elements_.eccentricity * sinE * deep_.xnodp / sqrt(1.0 - elements_.eccentricity * elements_.eccentricity);
+    const double rdot = deep_.xnodp * deep_.aodp * elements_.eccentricity * sinE / sqrt(1.0 - elements_.eccentricity * elements_.eccentricity);
     const double rfdot = deep_.xnodp * deep_.aodp * sqrt(1.0 - elements_.eccentricity * elements_.eccentricity) / r;
 
     // Ориентация
-    const double cosw = cos(omgadf);
-    const double sinw = sin(omgadf);
+    const double cosomg = cos(omgadf);
+    const double sinomg = sin(omgadf);
     const double cosnod = cos(xnode);
     const double sinnod = sin(xnode);
 
-    // Позиция в ECI (km)
-    const double sinv = (sqrt(1.0 - elements_.eccentricity * elements_.eccentricity) * sinE) / (1.0 - elements_.eccentricity * cosE);
-    const double cosv = (cosE - elements_.eccentricity) / (1.0 - elements_.eccentricity * cosE);
-
-    const double rx = r * (cosw * cosv - sinw * sinv);
-    const double ry = r * (sinw * cosv + cosw * sinv);
-    const double rz = r * sinv * deep_.sinio;
+    // Позиция и скорость в ECI
+    const double xmx = r * (cosomg * cosv - sinomg * sinv);
+    const double xmy = r * (sinomg * cosv + cosomg * sinv);
 
     state.position = QVector3D(
-        XKMPER * (rx * cosnod - ry * sinnod * deep_.cosio),
-        XKMPER * (rx * sinnod + ry * cosnod * deep_.cosio),
-        XKMPER * ry * deep_.sinio
+        XKMPER * (xmx * cosnod - xmy * sinnod * deep_.cosio),
+        XKMPER * (xmx * sinnod + xmy * cosnod * deep_.cosio),
+        XKMPER * xmy * deep_.sinio
         );
 
-    // Скорость в ECI (km/s)
-    const double rdotk = rdot * XKMPER / 60.0;
-    const double rfdotk = rfdot * XKMPER / 60.0;
+    // Преобразование скорости в км/с
+    const double vx = rdot * (cosomg * cosv - sinomg * sinv) -
+                      rfdot * (cosomg * sinv + sinomg * cosv);
+    const double vy = rdot * (sinomg * cosv + cosomg * sinv) -
+                      rfdot * (sinomg * sinv - cosomg * cosv);
 
     state.velocity = QVector3D(
-        rdotk * (cosw * cosv - sinw * sinv) * cosnod -
-            rfdotk * (sinw * cosv + cosw * sinv) * cosnod -
-            (rdotk * (cosw * cosv - sinw * sinv) * sinnod -
-             rfdotk * (sinw * cosv + cosw * sinv) * sinnod) * deep_.cosio,
-
-        rdotk * (cosw * cosv - sinw * sinv) * sinnod +
-            rfdotk * (sinw * cosv + cosw * sinv) * sinnod +
-            (rdotk * (cosw * cosv - sinw * sinv) * cosnod -
-             rfdotk * (sinw * cosv + cosw * sinv) * cosnod) * deep_.cosio,
-
-        rdotk * sinv * deep_.sinio + rfdotk * cosv * deep_.sinio
+        (XKMPER/60.0) * (vx * cosnod - vy * sinnod * deep_.cosio),
+        (XKMPER/60.0) * (vx * sinnod + vy * cosnod * deep_.cosio),
+        (XKMPER/60.0) * vy * deep_.sinio
         );
 
     return state;
--- src/sgp4_propagator.h
+++ src/sgp4_propagator.h
@@ -30,24 +30,32 @@ public:
     static GeodeticCoords eci2geodetic(const QVector3D& eci_pos, const QDateTime& time);
 
 private:
-    // WGS-72 constants
-    // Обновленные константы
+    // Константы
     static constexpr double XKE = 7.43669161e-2;
     static constexpr double CK2 = 5.413080e-4;
     static constexpr double CK4 = 0.62098875e-6;
-    static constexpr double XKMPER = 6378.135;
+    static constexpr double XKMPER = 6378.135;         // Earth radius km
     static constexpr double AE = 1.0;
-    static constexpr double XMNPDA = 1440.0;
-    static constexpr double OMEGA_E = 7.29211514670698e-5; // рад/с
-    static constexpr double TWOPI = 2.0 * M_PI;           // Добавлена константа TWOPI
+    static constexpr double XMNPDA = 1440.0;          // Minutes per day
+    static constexpr double OMEGA_E = 7.29211514670698e-5;  // Earth rotation rate rad/s
+    static constexpr double TWOPI = 2.0 * M_PI;
+    static constexpr double E6A = 1.0e-6;
+    static constexpr double TOTHRD = 2.0/3.0;
 
     struct DeepSpaceElements {
         double aodp, cosio, sinio, omgdot, xnodot, xmdot;
-        double xnodp, z1, z7, d1, d2, d3, d4, d5;
-        double del1, del2, del3;
+        double xnodp, theta2, x3thm1, xlcof, aycof;
+        double x1mth2, xmdot_factor, omgdot_factor, xnodot_factor;
     };
 
     DeepSpaceElements deep_;
+
+    // Вспомогательные функции
+    static double FMod2p(double x) {
+        double rv = fmod(x, TWOPI);
+        if (rv < 0.0) rv += TWOPI;
+        return rv;
+    }
 
     struct OrbitalElements {
         double inclination;     // радианы
